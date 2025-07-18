05620c13fca9131a9cfbd3029436059e9ffeb152
IOPress <iprogrammer@iopress.info>
2025-07-18 18:59:42 +0100

    Update phase2.py

--- C4/phase2/phase2.py
+++ C4/phase2/phase2.py
@@ -5,9 +5,8 @@ def gpio_get():
     return machine.mem32[0xd0000000+0x010]
 
 def gpio_set(value,mask): 
-    # 0x01C for Pico 0x028 for Pico 2
-    machine.mem32[0xd0000000+0x028]= (machine.mem32[0xd0000000+0x010])^value & mask
-
+    machine.mem32[0xd0000000+0x010] =
+         machine.mem32[0xd0000000+0x010] & ~mask | value & mask
 
 pin=Pin(22,Pin.OUT)
 pin=Pin(21,Pin.OUT)
